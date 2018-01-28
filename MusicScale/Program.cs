c8df7576e571ebd06d9e24c5e360c53615523288
Felipe Almeida <felipepereira937@gmail.com>
2018-01-28 13:24:29 -0200

    Adicionar arquivos de projeto.

---
+++ MusicScale/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
